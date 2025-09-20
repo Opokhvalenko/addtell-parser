493a793cc17f9979d0b3ef263ce7d166066267a5
Oksana <oksanapokhvalenko@gmail.com>
2025-09-20 21:24:07 +0300

    fix

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,10 @@ import { fileURLToPath } from "node:url";
 import AutoLoad from "@fastify/autoload";
 import Sensible from "@fastify/sensible";
 import Fastify, { type FastifyServerOptions } from "fastify";
+
 import configPlugin from "./config/index.js";
+import { ajvFormatsPlugin } from "./lib/ajvFormatsPlugin.js";
+import errorHandlerPlugin from "./plugins/error-handler.js";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 export type AppOptions = Partial<FastifyServerOptions>;
@@ -11,10 +14,7 @@ export type AppOptions = Partial<FastifyServerOptions>;
 async function buildApp(options: AppOptions = {}) {
   const isDev = process.env.NODE_ENV !== "production";
 
-  const { logger: _ignoreLogger, ...rest } = options;
-
   const fastify = Fastify({
-    ...(rest as FastifyServerOptions),
     logger: isDev
       ? {
           level: "info",
@@ -24,11 +24,22 @@ async function buildApp(options: AppOptions = {}) {
           },
         }
       : { level: "info" },
+
+    ajv: {
+      customOptions: {
+        allErrors: true,
+        coerceTypes: true,
+        removeAdditional: true,
+      },
+      plugins: [ajvFormatsPlugin],
+    },
+
+    ...options,
   });
 
   await fastify.register(configPlugin);
-
   await fastify.register(Sensible);
+  await fastify.register(errorHandlerPlugin);
 
   await fastify.register(AutoLoad, {
     dir: join(__dirname, "plugins"),
@@ -40,10 +51,11 @@ async function buildApp(options: AppOptions = {}) {
     dir: join(__dirname, "routes"),
     encapsulate: false,
     ignorePattern: /(^_|\.d\.ts$)/i,
+    routeParams: true,
+    maxDepth: 2,
   });
 
   fastify.get("/", async () => ({ hello: "world" }));
-
   return fastify;
 }
 
