04b6727366d146bc95b966d9fc7bbcea9602fcc8
Oksana <oksanapokhvalenko@gmail.com>
2025-09-27 15:10:16 +0300

    ci(render): деплой бекенду на Render + MongoDB Atlas

--- services/mongo/init-rs.js
+++
@@ -1,7 +0,0 @@
-try {
-  rs.status();
-  print("Replica set already initiated");
-} catch (e) {
-  print("Initiating replica set rs0...");
-  rs.initiate({ _id: "rs0", members: [{ _id: 0, host: "localhost:27017" }] });
-}
