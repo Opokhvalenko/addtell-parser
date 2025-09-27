76bbcc6719055f4a9ac876ba7247e096e3be61d7
Oksana <oksanapokhvalenko@gmail.com>
2025-09-27 12:13:13 +0300

    update render.yaml

--- services/mongo/init-rs.js
+++ services/mongo/init-rs.js
@@ -1,10 +1,7 @@
 try {
   rs.status();
-  print("Replica set already exists");
+  print("Replica set already initiated");
 } catch (e) {
-  rs.initiate({
-    _id: "rs0",
-    members: [{ _id: 0, host: "localhost:27017" }],
-  });
-  print("Replica set initiated: rs0");
+  print("Initiating replica set rs0...");
+  rs.initiate({ _id: "rs0", members: [{ _id: 0, host: "localhost:27017" }] });
 }
