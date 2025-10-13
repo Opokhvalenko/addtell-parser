4580e118ed0ff68ab219d79fb9e19c391f4ce4f9
Oksana <oksanapokhvalenko@gmail.com>
2025-10-13 10:05:19 +0300

    update auth.plugin analytics/routes

--- src/plugins/auth.plugin.ts
+++ src/plugins/auth.plugin.ts
@@ -3,7 +3,7 @@ import authRoutes from "../modules/auth/routes.js";
 
 export default fp(
   async (app) => {
-    await app.register(authRoutes, { prefix: "/api" });
+    await app.register(authRoutes, { prefix: "/api/auth" });
     app.log.info("Auth plugin registered");
   },
   { name: "auth-routes", dependencies: ["jwt"] },
